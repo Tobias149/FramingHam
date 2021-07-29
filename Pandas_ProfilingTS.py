0ce41cc0ff5941360822fa2de026df2db58ac998
Tobias149 <85512614+Tobias149@users.noreply.github.com>
2021-07-29 21:30:11 +0200

    Update Pandas_ProfilingTS.py

--- Pandas_ProfilingTS.py
+++ Pandas_ProfilingTS.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from pandas_profiling import ProfileReport
 
-df = pd.read_csv('/Users/tobiasschmidt/Desktop/TechLabs 2/Dataset 2/Cleaned_framingham.csv')
+df = pd.read_csv('/Users/tobiasschmidt/Desktop/TechLabs 2/Dataset 2/framingham_cleaned_280721.csv')
 
 profile = ProfileReport(df, title="Pandas Profiling Report")
 
