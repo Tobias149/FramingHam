acdf5f98f8461e51b52cbf1cef0e2afa06bbf4f8
Tobias149 <85512614+Tobias149@users.noreply.github.com>
2021-07-29 22:05:11 +0200

    Update Pandas_ProfilingTS.py

--- Pandas_ProfilingTS.py
+++ Pandas_ProfilingTS.py
@@ -4,6 +4,6 @@ from pandas_profiling import ProfileReport
 
 df = pd.read_csv('/Users/tobiasschmidt/Desktop/TechLabs 2/Dataset 2/framingham_cleaned_280721.csv')
 
-profile = ProfileReport(df, title="Pandas Profiling Report")
+profile = ProfileReport(df, title="Framingham Profiling Report")
 
-profile.to_file("your_report.html")
+profile.to_file("Framingham_Report.html")
