1f906cf2431d3282554223c84150d296fce378e9
Tobias149 <85512614+Tobias149@users.noreply.github.com>
2021-08-04 14:33:11 +0200

    Delete Pandas_ProfilingTS.py

--- Pandas_ProfilingTS.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from pandas_profiling import ProfileReport
-
-df = pd.read_csv('/Users/tobiasschmidt/Desktop/TechLabs 2/Dataset 2/framingham_cleaned_280721.csv')
-
-profile = ProfileReport(df, title="Framingham Profiling Report")
-
-profile.to_file("Framingham_Report.html")
