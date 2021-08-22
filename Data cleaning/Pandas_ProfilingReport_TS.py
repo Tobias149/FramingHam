aa9d3dffa05c0fd0b93ae8f92769bd77c908e991
Tobias149 <85512614+Tobias149@users.noreply.github.com>
2021-08-22 11:58:41 +0200

    Add files via upload

---
+++ Data cleaning/Pandas_ProfilingReport_TS.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from pandas_profiling import ProfileReport
+
+# Load the dataset
+df = pd.read_csv('/Users/tobiasschmidt/Desktop/TechLabs 2/Dataset 2/framingham_origin.csv')
+
+# Load data into the Report
+profile = ProfileReport(df, title="Framingham Profiling Report")
+
+# Creation of the file: profile.to_file("your_report.html")
+#profile.to_file("Profiling_Report.html")
+profile.to_file("Profiling_Report.html")
+
+print("The report was created successfully! Look into your files")
